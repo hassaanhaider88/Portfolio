20c80cf1a4b32b31776bb3c19df55556b1d77bd0
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-03-13 21:43:14 +0500

    adding project now 12 LMS

--- ProjectsData.js
+++ ProjectsData.js
@@ -15,12 +15,32 @@ const ProjectsData = [
     ],
     links: {
       github: "https://github.com/hassaanhaider088/Repo.ReadMe",
-      live: "https://reporeadme.netlify.app/",
+      live: "https://reporeademe.org",
+    },
+  },
+  {
+    number: "002",
+    title: "LMS System Final Project",
+    date: "March 2026",
+    description:
+      "Final Project at Hunarmand Punjab Complete LMS System with Role base authentication and autharization with three roles Admin, Instructor and Student.",
+    technologies: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "Tailwindcss",
+      "react-icons",
+      "Context api",
+      "MongoDB",
+    ],
+    links: {
+      github: "https://github.com/hassaanhaider088/FinalProject",
+      live: "https://lms-by-hassaan.netlify.app/",
     },
   },
 
   {
-    number: "002",
+    number: "003",
     title: "SEO Wala AI",
     date: "4 Feb 2026",
     description:
@@ -40,7 +60,7 @@ const ProjectsData = [
     },
   },
   {
-    number: "003",
+    number: "004",
     title: "Get URI",
     date: "20 June 2025",
     description:
@@ -58,7 +78,7 @@ const ProjectsData = [
     },
   },
   {
-    number: "004",
+    number: "005",
     title: "APEX AI",
     date: "22 Dec 2025",
     description:
@@ -75,37 +95,14 @@ const ProjectsData = [
       live: "https://apexai-pk.netlify.app/",
     },
   },
-  {
-    number: "005",
-    title: "PersonaAEye AI",
-    date: "13 June 2025",
-    description:
-      "AI-powered frontend experience built with React, Zustand, and Tailwind CSS focused on performance and clean state management.",
-    technologies: [
-      "React.js",
-      "Node.js",
-      "Express.js",
-      "MongoDB",
-      "Tailwind CSS"
-    ],
-    links: {
-      github: "https://github.com/hassaanhaider88/PersonaAEye",
-      live: "https://personaaeye.netlify.app/",
-    },
-  },
+
   {
     number: "006",
     title: "Pinterest Clone",
     date: "10 Jan 2025",
     description:
       "Full-stack Pinterest-inspired platform developed with Node.js, Express, MongoDB, and EJS featuring authentication and pin management.",
-    technologies: [
-      "Node.js",
-      "Express.js",
-      "MongoDB",
-      "EJS",
-      "Tailwind CSS",
-    ],
+    technologies: ["Node.js", "Express.js", "MongoDB", "EJS", "Tailwind CSS"],
     links: {
       github: "https://github.com/hassaanhaider88/PersonaAEye",
       live: "https://pinsyourinterest.onrender.com",
@@ -117,10 +114,7 @@ const ProjectsData = [
     date: "11 Aug 2025",
     description:
       "Open-source React component library designed for beginners to learn and contribute real-world UI components.",
-    technologies: [
-      "React.js",
-      "Tailwind CSS",
-    ],
+    technologies: ["React.js", "Tailwind CSS"],
     links: {
       github: "https://github.com/hassaanhaider88/ButtonJs",
       live: "https://buttonjs.vercel.app",
@@ -132,13 +126,7 @@ const ProjectsData = [
     date: "2 June 2025",
     description:
       "Production-ready business website built with Vanilla JavaScript and a custom Node.js backend using MongoDB.",
-    technologies: [
-      "Node.js",
-      "MongoDB",
-      "Tailwind CSS",
-      "Cloudinary",
-
-    ],
+    technologies: ["Node.js", "MongoDB", "Tailwind CSS", "Cloudinary"],
     links: {
       github: "https://github.com/hassaanhaider88/GulzarSons",
       live: "http://gulzarsonsfurniture.com",
@@ -168,11 +156,7 @@ const ProjectsData = [
     date: "20 Dec 2023",
     description:
       "Fully responsive e-commerce application built with React, Tailwind CSS, and Context API including cart and product management.",
-    technologies: [
-      "React.js",
-      "Tailwind CSS",
-      "Context API",
-    ],
+    technologies: ["React.js", "Tailwind CSS", "Context API"],
     links: {
       github: "",
       live: "https://hmk-shop-by-hassaan-haider.netlify.app/",
@@ -184,20 +168,30 @@ const ProjectsData = [
     date: "20 Feb 2024",
     description:
       "Modern blogging platform built with Next.js and Tailwind CSS featuring authentication and scalable content management.",
-    technologies: [
-      "Next.js",
-      "Tailwind CSS",
-    ],
+    technologies: ["Next.js", "Tailwind CSS"],
     links: {
       github: "https://github.com/hassaanhaider88/HMK_Blogs",
       live: "https://hmk-codeweb.netlify.app",
     },
   },
-
-
-
-
+  {
+    number: "011",
+    title: "PersonaAEye AI",
+    date: "13 June 2025",
+    description:
+      "AI-powered frontend experience built with React, Zustand, and Tailwind CSS focused on performance and clean state management.",
+    technologies: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Tailwind CSS",
+    ],
+    links: {
+      github: "https://github.com/hassaanhaider88/PersonaAEye",
+      live: "https://personaaeye.netlify.app/",
+    },
+  },
 ];
-
 
 export default ProjectsData;
